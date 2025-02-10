daa24df6b27d8583f5612d127baed670a6677763
Ozodbek <ahmadovozodbek80@gmail.com>
2025-02-10 21:45:18 +0500

    Admin panel inter-face updated! :)

--- teachers/admin.py
+++ teachers/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
+from import_export.admin import ExportMixin
 from .models import Teacher
 
 
 @admin.register(Teacher)
-class TeacherAdmin(admin.ModelAdmin):
+class TeacherAdmin(ExportMixin, admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'phone', 'department', 'employment_type', 'join_date', 'is_active')
     search_fields = ('first_name', 'last_name', 'email', 'phone', 'department__name')
-    list_filter = ('employment_type', 'department', 'is_active')
+    list_filter = ('employment_type', 'department', 'is_active', 'join_date')
     ordering = ('last_name', 'first_name')
-    filter_horizontal = ('subjects',)
+    filter_horizontal = ('subjects',)  # ManyToManyField uchun yaxshiroq UI
+    list_editable = ('is_active',)  # Admin sahifasidan bevosita statusni o‘zgartirish
+    list_per_page = 20  # Har bir sahifada 20 ta ma’lumot chiqarish
+    date_hierarchy = 'join_date'  # Sanalar bo‘yicha saralash
+    readonly_fields = ('join_date',)  # Ishga kirgan sanani faqat ko‘rish mumkin
